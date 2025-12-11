a6a6f4599f4d445d9d0fe48374714bbfc3bace56
Lucile-luh <lucilemuchemwa374@gmail.com>
2025-12-11 13:22:59 +0200

    created two views

---
+++ MCRI Monitoring App/interns.swift
@@ -0,0 +1,18 @@
+//
+//  interns.swift
+//  MCRI Monitoring App
+//
+//  Created by LUCILE G MUCHEMWA on 11/12/2025.
+//
+
+import SwiftUI
+
+struct interns: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    interns()
+}
