5bf4f7b7a3c7d3d085d021292d67f214abef2148
Amanda <soganileamanda@gmail.com>
2025-12-11 14:01:17 +0200

    Created horizontal view for weeks

--- MCRI Monitoring App/Updates/HorizontalWeekView.swift
+++ MCRI Monitoring App/Updates/HorizontalWeekView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct HorizontalWeekView: View {
-    var week : String = ""
+    var week : String = "21/02/05"
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 20)
                 .frame(width: 350, height: 170)
-                .foregroundStyle(Color.gray)
+                .foregroundStyle(Color.gray.opacity(0.2))
             
-            
-            
+            Text("Week ending: \(week)")
+                .font(.title2)
         }
     }
 }
