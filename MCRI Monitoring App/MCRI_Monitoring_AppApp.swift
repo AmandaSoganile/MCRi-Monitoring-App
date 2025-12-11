ebb8299cd7898048869a913c34d3cd8368c34995
Amanda <soganileamanda@gmail.com>
2025-12-11 11:38:26 +0200

    Initial Commit

---
+++ MCRI Monitoring App/MCRI_Monitoring_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCRI_Monitoring_AppApp.swift
+//  MCRI Monitoring App
+//
+//  Created by Amanda Soganile on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MCRI_Monitoring_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
