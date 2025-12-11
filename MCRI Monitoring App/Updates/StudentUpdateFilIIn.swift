12cc879004ef9ee10a7471ffec606b40004af9e4
Amanda <soganileamanda@gmail.com>
2025-12-11 11:59:56 +0200

    Updates page

---
+++ MCRI Monitoring App/Updates/StudentUpdateFilIIn.swift
@@ -0,0 +1,18 @@
+//
+//  StudentUpdateFilIIn.swift
+//  MCRI Monitoring App
+//
+//  Created by Amanda Soganile on 11/12/2025.
+//
+
+import SwiftUI
+
+struct StudentUpdateFilIIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentUpdateFilIIn()
+}
