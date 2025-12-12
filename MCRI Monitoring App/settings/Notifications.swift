70ef2b1022c8241378a1648e1ca520fbb8827b5b
Ron <ronshadreck@gmail.com>
2025-12-12 04:31:28 +0200

    Added settings pages

---
+++ MCRI Monitoring App/settings/Notifications.swift
@@ -0,0 +1,18 @@
+//
+//  Notifications.swift
+//  MCRI Monitoring App
+//
+//  Created by LUCILE G MUCHEMWA on 11/12/2025.
+//
+
+import SwiftUI
+
+struct Notifications: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Notifications()
+}
---
+++ MCRI Monitoring App/settings/Support.swift
@@ -0,0 +1,18 @@
+//
+//  Support.swift
+//  MCRI Monitoring App
+//
+//  Created by LUCILE G MUCHEMWA on 11/12/2025.
+//
+
+import SwiftUI
+
+struct Support: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Support()
+}
