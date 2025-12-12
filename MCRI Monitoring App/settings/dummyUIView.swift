ec74fd8fee958386662ed0556e236606e9ee738e
Lucile-luh <lucilemuchemwa374@gmail.com>
2025-12-12 03:44:56 +0200

    added logic to the password page

---
+++ MCRI Monitoring App/settings/dummyUIView.swift
@@ -0,0 +1,18 @@
+//
+//  dummyUIView.swift
+//  MCRI Monitoring App
+//
+//  Created by LUCILE G MUCHEMWA on 12/12/2025.
+//
+
+import SwiftUI
+
+struct dummyUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dummyUIView()
+}
