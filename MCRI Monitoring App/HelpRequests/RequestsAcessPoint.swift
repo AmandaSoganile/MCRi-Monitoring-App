7d3bcd179d7a97c0dc5b0a2edf527e094f9e69da
Lucile-luh <lucilemuchemwa374@gmail.com>
2025-12-12 09:06:52 +0200

    refactored

---
+++ MCRI Monitoring App/HelpRequests/RequestsAcessPoint.swift
@@ -0,0 +1,18 @@
+//
+//  RequestsAcessPoint.swift
+//  MCRI Monitoring App
+//
+//  Created by LUCILE G MUCHEMWA on 12/12/2025.
+//
+
+import SwiftUI
+
+struct RequestsAcessPoint: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    RequestsAcessPoint()
+}
